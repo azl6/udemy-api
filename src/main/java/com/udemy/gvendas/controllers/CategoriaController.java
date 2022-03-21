7506ac8bd6c807a1f572b9420478dc5869cab2b5
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-03-21 16:03:43 -0300

    Documentando a API com Swagger

--- src/main/java/com/udemy/gvendas/controllers/CategoriaController.java
+++ src/main/java/com/udemy/gvendas/controllers/CategoriaController.java
@@ -2,6 +2,8 @@ package com.udemy.gvendas.controllers;
 
 import com.udemy.gvendas.domain.Categoria;
 import com.udemy.gvendas.services.CategoriaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = "Categoria")
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
@@ -17,24 +20,28 @@ public class CategoriaController {
     @Autowired
     private CategoriaService service;
 
+    @ApiOperation(value = "Listar todas")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Categoria>> findAll(){
         List<Categoria> obj =  service.findAll();
         return ResponseEntity.ok().body(obj);
     }
 
+    @ApiOperation(value = "Find by id")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Categoria> findById(@PathVariable Long id){
         Categoria obj =  service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @ApiOperation(value = "Insert")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody Categoria categoria){
         service.save(categoria);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ApiOperation(value = "Update")
     @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
     public ResponseEntity<Void> update(@Valid @PathVariable Long id, @RequestBody Categoria categoria){
         service.update(id, categoria);
