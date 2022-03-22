84bfbbc549408d9da3f47afa7c357a4da9397ab1
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-03-22 16:21:52 -0300

    Resolução do desafio: Criando endpoints para listar produtos

--- src/main/java/com/udemy/gvendas/controllers/ProdutoController.java
+++ src/main/java/com/udemy/gvendas/controllers/ProdutoController.java
@@ -1,14 +1,36 @@
 package com.udemy.gvendas.controllers;
 
 import com.udemy.gvendas.domain.Categoria;
+import com.udemy.gvendas.domain.Produto;
 import com.udemy.gvendas.repositories.CategoriaRepository;
 import com.udemy.gvendas.repositories.ProdutoRepository;
+import com.udemy.gvendas.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@RequestMapping(value = "/produtos")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Produto>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
 
 
 }
--- src/main/java/com/udemy/gvendas/services/ProdutoService.java
+++ src/main/java/com/udemy/gvendas/services/ProdutoService.java
@@ -1,7 +1,26 @@
 package com.udemy.gvendas.services;
 
+import com.udemy.gvendas.domain.Produto;
+import com.udemy.gvendas.repositories.ProdutoRepository;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repo;
+
+    public List<Produto> findAll(){
+        return repo.findAll();
+    }
+
+    public Produto findById(Long id){
+        Optional<Produto> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(1, "Objeto não encontrado"));
+    }
 }
