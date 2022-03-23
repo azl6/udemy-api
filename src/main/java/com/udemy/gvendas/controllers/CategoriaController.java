3412ae7369223ded1f9f090a231f99ff9897b2cd
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-03-23 13:58:41 -0300

    Ajustes visuais no Swagger

--- src/main/java/com/udemy/gvendas/controllers/CategoriaController.java
+++ src/main/java/com/udemy/gvendas/controllers/CategoriaController.java
@@ -2,6 +2,7 @@ package com.udemy.gvendas.controllers;
 
 import com.udemy.gvendas.domain.Categoria;
 import com.udemy.gvendas.services.CategoriaService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,30 +18,35 @@ public class CategoriaController {
     @Autowired
     private CategoriaService service;
 
+    @ApiOperation(value = "Listar todas as categorias")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Categoria>> findAll(){
         List<Categoria> obj =  service.findAll();
         return ResponseEntity.ok().body(obj);
     }
 
+    @ApiOperation(value = "Encontrar categoria por id")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Categoria> findById(@PathVariable Long id){
         Categoria obj =  service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @ApiOperation(value = "Inserir categoria")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody Categoria categoria){
         service.save(categoria);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ApiOperation(value = "Atualizar categoria")
     @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
     public ResponseEntity<Void> update(@Valid @PathVariable Long id, @RequestBody Categoria categoria){
         service.update(id, categoria);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ApiOperation(value = "Deletar categoria")
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
--- src/main/java/com/udemy/gvendas/controllers/ProdutoController.java
+++ src/main/java/com/udemy/gvendas/controllers/ProdutoController.java
@@ -5,6 +5,7 @@ import com.udemy.gvendas.domain.Produto;
 import com.udemy.gvendas.repositories.CategoriaRepository;
 import com.udemy.gvendas.repositories.ProdutoRepository;
 import com.udemy.gvendas.services.ProdutoService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,16 +21,19 @@ public class ProdutoController {
     @Autowired
     private ProdutoService service;
 
+    @ApiOperation(value = "Listar todos os produtos")
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Produto>> findAll(){
         return ResponseEntity.ok().body(service.findAll());
     }
 
+    @ApiOperation(value = "Encontar produto por id")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Produto> findById(@PathVariable Long id){
         return ResponseEntity.ok().body(service.findById(id));
     }
 
+    @ApiOperation(value = "Inserir produto")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Produto> save(@Valid @RequestBody Produto produto){
         return ResponseEntity.ok().body(service.insert(produto));
