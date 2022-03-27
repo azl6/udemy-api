155dd66e8a1a2603c9c01754bfc16a6b702a27af
Alex <alex.rodrigues23997@gmail.com>
2022-03-27 16:06:14 -0300

    Adicionando Bean Validation ao ClienteRequestDTO

--- src/main/java/com/udemy/gvendas/dto/Cliente/ClienteRequestDTO.java
+++ src/main/java/com/udemy/gvendas/dto/Cliente/ClienteRequestDTO.java
@@ -4,20 +4,34 @@ import com.udemy.gvendas.domain.Cliente;
 import com.udemy.gvendas.domain.Endereco;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @ApiModel("Cliente requisição DTO")
 public class ClienteRequestDTO {
 
     @ApiModelProperty(value = "nome")
+    @NotBlank(message = "O campo NOME deve ser preenchido")
+    @Length(min = 3, max = 50, message = "O campo NOME deve ter entre 3 e 50 caracteres")
     private String nome;
 
+
     @ApiModelProperty(value = "telefone")
+    @NotBlank(message = "O campo TELEFONE deve ser preenchido")
+    @Pattern(regexp = "\\([\\d]{2}\\)[\\d]{5}[- .][\\d]{4}", message = "O campo TELEFONE deve seguir o seguinte padrão: (xx)xxxxx-xxxx")
     private String telefone;
 
     @ApiModelProperty(value = "ativo")
+    @NotNull(message = "O campo ATIVO não pode ser nulo")
     private Boolean ativo;
 
     @ApiModelProperty(value = "endereco")
+    @NotNull
+    @Valid
     private EnderecoRequestDTO enderecoDto;
 
     public Cliente converterParaEntidade(){
--- src/main/java/com/udemy/gvendas/dto/Cliente/EnderecoRequestDTO.java
+++ src/main/java/com/udemy/gvendas/dto/Cliente/EnderecoRequestDTO.java
@@ -2,29 +2,45 @@ package com.udemy.gvendas.dto.Cliente;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @ApiModel("Endereço requisição DTO")
 public class EnderecoRequestDTO {
 
     @ApiModelProperty(value = "logradouro")
+    @NotBlank(message = "O campo LOGRADOURO deve ser preenchido")
+    @Length(min = 3, max = 30, message = "O campo LOGRADOURO deve ter entre 3 e 30 caracteres")
     private String logradouro;
 
     @ApiModelProperty(value = "numero")
+    @NotNull(message = "O campo NÚMERO não pode ser nulo")
     private Integer numero;
 
     @ApiModelProperty(value = "complemento")
+    @Length(max = 30, message = "O campo LOGRADOURO deve ter no máximo 30 caracteres")
     private String complemento;
 
     @ApiModelProperty(value = "bairro")
+    @Length(min = 3, max = 30, message = "O campo BAIRRO deve ter entre 3 e 30 caracteres")
     private String bairro;
 
     @ApiModelProperty(value = "cep")
+    @NotBlank(message = "O campo CEP deve ser preenchido")
+    @Pattern(regexp = "[\\d]{5}-[\\d]{3}", message = "O campo CEP deve ter o seguinte padrão: xxxxx-xxx")
     private String cep;
 
     @ApiModelProperty(value = "cidade")
+    @NotBlank(message = "O campo CIDADE deve ser preenchido")
+    @Length(min = 3, max = 30, message = "O campo CIDADE deve ter entre 3 e 30 caracteres")
     private String cidade;
 
     @ApiModelProperty(value = "estado")
+    @NotBlank(message = "O campo ESTADO deve ser preenchido")
+    @Length(min = 3, max = 30, message = "O campo ESTADO deve ter entre 3 e 30 caracteres")
     private String estado;
 
     public String getLogradouro() {
