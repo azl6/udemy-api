395a7f056fa5df9fbd38e03eb0ecbd62bab5f0bd
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-03-28 13:48:24 -0300

    Mapeando Venda e ItemVenda com JPA

---
+++ src/main/java/com/udemy/gvendas/domain/ItemVenda.java
@@ -0,0 +1,68 @@
+package com.udemy.gvendas.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "item_venda")
+public class ItemVenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "preco_vendido")
+    private BigDecimal precoVendido;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_produto", referencedColumnName = "codigo")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_venda", referencedColumnName = "codigo")
+    private Venda venda;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoVendido() {
+        return precoVendido;
+    }
+
+    public void setPrecoVendido(BigDecimal precoVendido) {
+        this.precoVendido = precoVendido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+}
---
+++ src/main/java/com/udemy/gvendas/domain/Venda.java
@@ -0,0 +1,45 @@
+package com.udemy.gvendas.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "venda")
+public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long codigo;
+
+    @Column(name = "data")
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_cliente", referencedColumnName = "codigo")
+    private Cliente cliente;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
